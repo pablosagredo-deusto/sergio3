ef3c3840a44d081ae67925235d96f3e9964ab5f6
Pablo <pablo.sagredo@opendeusto.es>
2022-04-02 17:58:13 +0200

    Interface de login

--- src/main/java/com/example/spq2/LoginApplication.java
+++ src/main/java/com/example/spq2/LoginApplication.java
@@ -12,7 +12,7 @@ public class LoginApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Login");
+        stage.setTitle("Login prueba");
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
